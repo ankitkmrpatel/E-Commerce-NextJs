1b5d2c704f27d34edb4d5ac4209ebf9fabb99d7a
ankitkmrpatel <ankitpatel14031@gmail.com>
2023-03-07 03:10:32 +0530

    Added Sanity Project

---
+++ sanityy-ecommerce/dist/static/index-d647773e-8535dd42.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-9e58f0f1-7bf30c02.js";import"./desk-257f76bb-54c14fbe.js";import"./index-8c757671.js";export{m as DocumentPaneProvider,a as default};
---
+++ sanityy-ecommerce/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '190vo6ys',
+    dataset: 'production'
+  }
+})
